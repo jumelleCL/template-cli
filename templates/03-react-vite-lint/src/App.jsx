8de2d1cd219228fcd74ea825f30c88a8d3fd03cf
ivluengo <ivluengo@gmail.com>
2024-02-28 18:03:19 +0100

    cambios

--- templates/03-react-vite-lint/src/App.jsx
+++ templates/03-react-vite-lint/src/App.jsx
@@ -1,9 +1,9 @@
-function App () {
-    return (
-        <>
-            <h1>Proyecto de React</h1>
-        </>
-    );
+function App() {
+  return (
+    <>
+      <h1>Proyecto de React</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ templates/12-react-vite-tailwind/src/App.jsx
@@ -0,0 +1,10 @@
+function App () {
+
+  return (
+    <>
+      <h1>Proyecto con React y Tailwind</h1>        
+    </>
+  );
+}
+
+export default App;
