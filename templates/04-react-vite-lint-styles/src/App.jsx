72c34efa20c8c3ed7382fd94b480ad1a45aec7d7
Camilo <jumellecamilo@gmail.com>
2024-11-04 21:19:08 +0100

    Cambios de templates

--- templates/04-react-vite-lint-styles/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className='center-vertical'>
-      <h1>Proyecto de React 🤘</h1>
-    </div>
-  );
-}
-
-export default App;
--- templates/06-react-vite-standard-styles/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="center-vertical">
-      <h1>Proyecto de React + Estilos</h1>
-    </div>
-  );
-}
-
-export default App;
