649606c0783e80675f075cde8435ed237ce63a7a
Camilo <jumellecamilo@gmail.com>
2024-11-04 21:31:13 +0100

    template bootstrap añadido

---
+++ templates/01-react-vite-ts-bootstrap/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center text-primary">React Vite + Typescript + Bootstrap</h1>
+    </div>
+  )
+}
+
+export default App
